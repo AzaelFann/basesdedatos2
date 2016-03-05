20fa6a725c518f0745da08fd4907e9b50690c438
AzaelFann <azaelshinigami@gmail.com>
2016-03-05 10:16:54 -0600

    Update trigger2.sql

--- trigger2.sql
+++ trigger2.sql
@@ -1,3 +1,27 @@
+create table registro(
+id_reg integer,
+nombre varchar2(40),
+mail varchar2(40),
+edad integer,
+constraint pk_id_reg primary key (id_reg)
+);
+
+create table registro_mayores(
+id_reg_may integer,
+nombre varchar2(40),
+mail varchar2(40),
+edad integer,
+constraint pk_id_reg_may primary key (id_reg_may));
+
+create table registro_menores(
+id_reg_men integer,
+nombre varchar2(40),
+mail varchar2(40),
+edad integer,
+constraint pk_id_reg_men primary key (id_reg_men));
+
+set serveroutput on;
+
 create or replace trigger respaldo_registro after insert into registro
 for each row
 begin
